ef190cb36541a26b83d1beefe417996531645785
Saral-Chauhangit <saralchauhan424t@gmail.com>
2025-02-24 13:58:45 +0530

    commit to the currentmonth bug fix

--- controllers/index.php
+++ controllers/index.php
@@ -13,10 +13,11 @@ $groupExpenses = $db->select("
     LEFT JOIN expenses e ON g.id = e.group_id
     GROUP BY g.id, g.group_name, g.created_at
 ")->get();
-$expenses = $db->select('SELECT e.id, e.title, e.amount, e.date, g.group_name AS category 
+$expenses = $db->query('SELECT e.id, e.title, e.amount, e.date, g.group_name AS category 
                         FROM expenses e
                         JOIN groups g ON e.group_id = g.id
-                        ORDER BY e.date DESC')->get();
+                        ORDER BY e.date DESC')->get() ?? 0;
+            
 
 
 $totalExpense = floor(array_sum(array_column($expenses,'amount')));
@@ -25,34 +26,8 @@ $currentMonth = date("Y-m"); // Get current month in "YYYY-MM" format
 
 $currentMonthExpenses = array_map(fn($e) => 
                                     (substr($e["date"], 0, 7)) === $currentMonth ? $e['amount'] : 0, $expenses);
-
-$totalCurrentMonth = array_sum($currentMonthExpenses);
-$maxCurrentMonth = max($currentMonthExpenses);
-
-
-
-
-
-// $sum = $db->query("SELECT SUM(amount) AS total FROM expenses")->findOrFail();
-// $max = $db->query("
-//     SELECT MAX(amount) AS total 
-//     FROM expenses 
-//     WHERE MONTH(date) = MONTH(CURRENT_DATE()) 
-//     AND YEAR(date) = YEAR(CURRENT_DATE())
-// ")->findOrFail();
-// $total = $db->query("
-//     SELECT SUM(amount) AS total 
-//     FROM expenses 
-//     WHERE MONTH(date) = MONTH(CURRENT_DATE()) 
-//     AND YEAR(date) = YEAR(CURRENT_DATE())
-// ")->findOrFail();
-// $group_total = $db->query("
-//     SELECT g.id AS group_id, COALESCE(SUM(e.amount), 0) AS total 
-//     FROM groups g
-//     LEFT JOIN expenses e ON g.id = e.group_id
-//     GROUP BY g.id
-// ")->get();
-
+$totalCurrentMonth = array_sum($currentMonthExpenses) ?? 0;
+$maxCurrentMonth = empty($currentMonthExpenses) ? 0 : max($currentMonthExpenses);
 
 view('index.view.php',[
     'groups'=> $groupExpenses,
